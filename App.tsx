94f74dc1274c635a16936affba822d469bef1e86
Roman <>
2024-06-18 13:07:33 +0300

    Added light/dark mode

--- App.tsx
+++ App.tsx
@@ -5,114 +5,58 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
+  Appearance,
+  Pressable,
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  useColorScheme
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { darkTheme, lightTheme } from './constants';
+import styles from './styles';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [isSwitchOn, setIsSwitchOn] = useState(isDarkMode);
+  const [currentTheme, setCurrentTheme] = useState(darkTheme);
+
+  useEffect(() => {
+    const colorScheme = Appearance.getColorScheme();
+    if (colorScheme === 'dark') {
+      setCurrentTheme(darkTheme);
+    } else {
+      setCurrentTheme(lightTheme);
+    }
+  }, []);
+
+  const toggleSwitch = () => {
+    setIsSwitchOn(!isSwitchOn);
+    const newTheme = isSwitchOn ? darkTheme : lightTheme;
+    setCurrentTheme(newTheme);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: currentTheme.containerBackgroundColor
+    }}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+        backgroundColor={currentTheme.containerBackgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text style={[styles.title, { color: currentTheme.textColor }]}>
+        Hello
+      </Text>
+      <Pressable style={currentTheme.switchButtonBackgroundColor} onPress={toggleSwitch}>
+        <Text style={{ color: currentTheme.textColor }}>Change Theme</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ constants.ts
@@ -0,0 +1,14 @@
+
+export const darkTheme = {
+  containerBackgroundColor: 'black',
+  containerViewColor: 'white',
+  textColor: 'white',
+  switchButtonBackgroundColor: '#ccc',
+};
+
+export const lightTheme = {
+  containerBackgroundColor: 'white',
+  containerViewColor: 'black',
+  textColor: 'black',
+  switchButtonBackgroundColor: '#ccc',
+};
